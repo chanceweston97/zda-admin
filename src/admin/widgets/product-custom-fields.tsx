1509549314181718f3c83a1a8842546eb3dd1979
Chance Weston <chanceweston97@gmail.com>
2025-12-09 13:37:20 -0600

    update admin

--- src/admin/widgets/product-custom-fields.tsx
+++ src/admin/widgets/product-custom-fields.tsx
@@ -178,7 +178,7 @@ const ProductCustomFields = () => {
       })
     }
 
-    // Run immediately and also after delay
+    // Run immediately 
     hideFields()
     setTimeout(hideFields, 1000)
     setTimeout(hideFields, 2000)
