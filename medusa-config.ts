b7b934faead07001468b35ec1eb6c1c6428b433b
Chance Weston <chanceweston97@gmail.com>
2025-12-10 12:34:25 -0600

    image issue

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,13 +2,15 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+// Get backend URL for file service
+const getBackendUrl = () => {
+  const baseUrl = process.env.MEDUSA_BACKEND_URL || process.env.BACKEND_URL || "http://localhost:9000"
+  return baseUrl.endsWith("/static") ? baseUrl : baseUrl + "/static"
+}
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    backend_url: (() => {
-      const baseUrl = process.env.MEDUSA_BACKEND_URL || process.env.BACKEND_URL || "http://localhost:9000"
-      return baseUrl.endsWith("/static") ? baseUrl : baseUrl + "/static"
-    })(),
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -20,6 +22,24 @@ module.exports = defineConfig({
   modules: [
     {
       resolve: "./src/modules/cable-customizer",
+    },
+    // Register File Module with backend_url option
+    // This is required for file uploads to use the correct server URL
+    // See: https://docs.medusajs.com/resources/infrastructure-modules/file/local
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-local",
+            id: "local",
+            options: {
+              upload_dir: "static",
+              backend_url: getBackendUrl(),
+            },
+          },
+        ],
+      },
     },
   ],
 })
