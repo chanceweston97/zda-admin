61644096e4d8e45bb6e8d99411d6aee588c66519
Chance Weston <chanceweston97@gmail.com>
2025-12-16 13:29:06 -0600

    t3 large

--- medusa-config.ts
+++ medusa-config.ts
@@ -17,7 +17,10 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+  },
+  admin: {
+    disable: true,
   },
   modules: [
     {
