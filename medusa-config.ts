67531db1f7d5a936c0256a136605f96cae82ecef
Chance Weston <chanceweston97@gmail.com>
2025-12-16 13:32:58 -0600

    t3 large

--- medusa-config.ts
+++ medusa-config.ts
@@ -19,9 +19,6 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  admin: {
-    disable: true,
-  },
   modules: [
     {
       resolve: "./src/modules/cable-customizer",
